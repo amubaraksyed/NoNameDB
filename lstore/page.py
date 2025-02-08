0f718f879d26d3261e85c9cd5767dbfe14b0641e
Jaynor Singson <jsingson6715@gmail.com>
2025-02-07 20:05:36 -0800

    Implemented db.py, unsuccessful page.py

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,17 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        if self.num_records < len(self.data):
+            return True
+        else:
+            return False
 
     def write(self, value):
-        self.num_records += 1
-        pass
+        # only write if bytearray has capacity
+        if self.has_capacity:
+            self.data[self.num_records] = value
+            self.num_records += 1
+        else:
+            print("Could not write")
+        
 
